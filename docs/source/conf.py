31b5a015a6cda5323bdbd0e92629bcce34a39a91
Marcos Deros <marcosderos@protonmail.com>
2025-01-27 21:28:31 +0100

    Try to ensure the correct path in deployment (#32)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -6,6 +6,11 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(".."))
+
 project = "Semantiva"
 copyright = "2025, Rafael Pezzi, Marcos Deros"
 author = "Rafael Pezzi, Marcos Deros"
