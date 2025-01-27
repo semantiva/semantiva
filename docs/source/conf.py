f7bdc8cfc7c5ad4f668eea1d96fa7e0263b4ef72
Marcos Deros <marcosderos78@gmail.com>
2025-01-27 22:38:35 +0100

    fix-yaml

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -9,7 +9,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.abspath(".."))
+sys.path.insert(0, os.path.abspath("../.."))
 
 project = "Semantiva"
 copyright = "2025, Rafael Pezzi, Marcos Deros"
