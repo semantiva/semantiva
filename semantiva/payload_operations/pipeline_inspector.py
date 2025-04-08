1ac78aee98f1599170c4d061c20b52ec7bdc41af
Marcos Deros <marcosderos78@gmail.com>
2025-04-08 22:23:30 +0200

    Added missing type hint

--- semantiva/payload_operations/pipeline_inspector.py
+++ semantiva/payload_operations/pipeline_inspector.py
@@ -188,7 +188,7 @@ class PipelineInspector:
         return "None"
 
     @staticmethod
-    def _format_context_params(context_params: set[str], key_origin) -> str:
+    def _format_context_params(context_params: set[str], key_origin: dict[str, int]) -> str:
         """
         Produce a string describing which context params a node requires, optionally including
         the node of origin if the parameter was created earlier in the pipeline.
