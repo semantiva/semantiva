b5557f47df4b73c56fd2f6966366c84646afa3ca
Rafael Peretti Pezzi <rafael.pezzi@rpezzi.com>
2025-08-27 11:49:57 +0200

    docs: update documentation build process; change build directory references from 'build' to '_build' and add YAML verification tool

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,3 +1,17 @@
+# Copyright 2025 Semantiva authors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 # Configuration file for the Sphinx documentation builder.
 import os
 import sys
@@ -13,6 +27,7 @@ release = "0.5.0"
 extensions = [
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
+    "sphinx.ext.doctest",
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
 ]
@@ -49,7 +64,7 @@ autodoc_type_aliases = {
     "X": "typing.Any",
     "Y": "typing.Any",
 }
-nitpicky = False
+nitpicky = True
 
 templates_path = ["_templates"]
 exclude_patterns = []
